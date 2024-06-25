8d00fa91e1b3df46061cb0de97f39f2dfe2ef9e1
goattoph <ruiafonsopt02@gmail.com>
2024-06-25 16:55:05 +0100

    uso de Regular Expressions no Telemóvel

--- Dweb-Projeto/Models/Utilizador.cs
+++ Dweb-Projeto/Models/Utilizador.cs
@@ -12,12 +12,24 @@ namespace Dweb_Projeto.Models{
         [Key] // PK
         public int UserID { get; set; }
 
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório!")]
+        [StringLength(50, ErrorMessage = "O {0} não pode ter mais de {1} caracteres.")]
         public string Nome { get; set; }
 
+        [Display(Name = "Foto")]
+        [Required(ErrorMessage = "A {0} é de preenchimento obrigatório!")]
+        [StringLength(50)]
         public string Foto { get; set;}
 
+        [Display(Name = "Data Nascimento")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
         public DateOnly DataNascimento { get; set; }
 
+        [Display(Name = "Telefone")]
+        [StringLength(19)]
+        [RegularExpression("([+]|00)?[0-9]{6,17}", ErrorMessage = "o {0} só pode conter digitos. No mínimo 6.")]
         public string Telefone { get; set; }
 
         /* ****************************************

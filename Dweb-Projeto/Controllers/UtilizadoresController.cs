3d88baed2e922fcb04c1ff91a915715d751bbb5a
goattoph <ruiafonsopt02@gmail.com>
2024-06-27 19:30:34 +0100

    Alterações no controller e views de "Utilizadores" para conseguir receber e mostrar imagens

--- Dweb-Projeto/Controllers/UtilizadoresController.cs
+++ Dweb-Projeto/Controllers/UtilizadoresController.cs
@@ -10,13 +10,21 @@ using Dweb_Projeto.Models;
 
 namespace Dweb_Projeto.Controllers
 {
-    public class UtilizadoresController : Controller
-    {
+    public class UtilizadoresController : Controller{
+        /// <summary>
+        /// referência à base de dados do projeto
+        /// </summary>
         private readonly ApplicationDbContext _context;
 
-        public UtilizadoresController(ApplicationDbContext context)
+        /// <summary>
+        /// objecto que contém os dados do Servidor
+        /// </summary>
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public UtilizadoresController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         // GET: Utilizadores
@@ -54,14 +62,104 @@ namespace Dweb_Projeto.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("UserID,Nome,Foto,DataNascimento,Telefone")] Utilizador utilizador)
-        {
-            if (ModelState.IsValid)
-            {
+        public async Task<IActionResult> Create([Bind("UserID,Nome,DataNascimento,Telefone")] Utilizador utilizador, IFormFile ImagemFoto){
+            // a anotação [Bind] informa o servidor de quais os atributos
+            // que devem ser lidos do objeto que vem do browser
+
+            /* Guardar a imagem no disco rígido do Servidor
+             * Algoritmo
+             * 1- há ficheiro?
+             *    1.1 - não
+             *          devolvo controlo ao browser
+             *          com mensagem de erro
+             *    1.2 - sim
+             *          Será imagem (JPG,JPEG,PNG)?
+             *          1.2.1 - não
+             *                  devolvo controlo ao browser
+             *                  com mensagem de erro
+             *          1.2.2 - sim
+             *                  - determinar o nome da imagem
+             *                  - guardar esse nome na BD
+             *                  - guardar o ficheir no disco rígido
+             */
+
+            // avalia se os dados recebido do browser estão
+            // de acordo com o Model
+
+            if (ModelState.IsValid){
+
+                // vars auxiliares
+                string nomeImagem = "";
+                bool haImagem = false;
+
+                // há ficheiro?
+                if (ImagemFoto == null)
+                {
+                    //não há, crio msg de erro
+                    ModelState.AddModelError("", "Deve fornecer uma imagem");
+                    //devolvo controlo ao browser
+                    return View(utilizador);
+                }
+                else
+                {
+                    // há ficheiro, será uma imagem?
+                    if (!(ImagemFoto.ContentType == "image/jpeg" ||
+                        ImagemFoto.ContentType == "image/jpg" ||
+                        ImagemFoto.ContentType == "image/png"))
+                    {
+                        //não é uma imagem
+                        ModelState.AddModelError("", "O ficheiro fornecido não é uma imagem válida");
+                        //devolvo controlo ao browser
+                        return View(utilizador);
+                    }
+                    else
+                    {
+                        // há imagem e é uma imagem válida
+                        haImagem = true;
+                        // determinar o nome da imagem
+                        Guid g = Guid.NewGuid();
+                        nomeImagem = g.ToString();
+                        string extensaoImagem = Path.GetExtension(ImagemFoto.FileName).ToLowerInvariant();
+                        nomeImagem += extensaoImagem;
+                        // guardar o nome da imagem na BD
+                        utilizador.Foto = nomeImagem;
+                    }
+                }
+                                
+                // adiciona à BD os dados vindos da View
                 _context.Add(utilizador);
+                // commit
                 await _context.SaveChangesAsync();
+
+                // guardar a imagem da foto no disco rígido
+                if (haImagem) {
+                    // determinar o local de armazenamento da imagem
+                    string localizacaoImagem = _webHostEnvironment.WebRootPath;
+                    // adicionar à raiz da parte web, o nome da pasta onde queremos guardar as imagens
+                    localizacaoImagem = Path.Combine(localizacaoImagem, "Imagens");
+
+                    // será que o local existe?
+                    if (!Directory.Exists(localizacaoImagem))
+                    {
+                        Directory.CreateDirectory(localizacaoImagem);
+                    }
+
+                    // atribuir ao caminho o nome da imagem
+                    localizacaoImagem = Path.Combine(localizacaoImagem, nomeImagem);
+
+                    // guardar a imagem no Disco Rígido
+                    using var stream = new FileStream(
+                       localizacaoImagem, FileMode.Create
+                       );
+                    await ImagemFoto.CopyToAsync(stream);
+                }
+
+                // redireciona o utilizador para a página de 'início'
+                // dos Cursos
                 return RedirectToAction(nameof(Index));
             }
+            // se cheguei aqui é pq alguma coisa correu mal
+            // devolve controlo à View, apresentando os dados recebidos
             return View(utilizador);
         }
 
